6c11752910a1644aa56cd139da298cdf3d0ad71d
Brent Perry <burentoperii@gmail.com>
2016-12-15 16:56:27 -0600

    add function to replace vowels with dashes.

--- js/scripts.js
+++ js/scripts.js
@@ -4,10 +4,24 @@ var toArray = function (string) {
   return string.split("");
 }
 
+var replaceVowels = function (array) {
+  var withDash = [];
+  for (var i = 0; i < array.length; i += 1) {
+    if (vowels.indexOf(array[i]) >= 0) {
+      withDash.push("-");
+    } else {
+      withDash.push(array[i]);
+    }
+  }
+  return withDash;
+}
+
+
+
 $(document).ready(function () {
   $("#input form").submit(function (event) {
     event.preventDefault();
     var phrase = $("#phrase").val();
-    console.log(toArray(phrase));
+    console.log(replaceVowels(toArray(phrase)));
   });
 });
